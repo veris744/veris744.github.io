f2a6e79dec8791de971926936cb53f25d4c77a8c
veris744 <71214562+veris744@users.noreply.github.com>
2025-04-28 18:06:51 +0200

    home structure full

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "339363104"
+    serviceWorkerVersion: "4170867480"
   }
 });
