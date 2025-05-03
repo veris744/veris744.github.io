9096a700e183ebc525b90e247110b86e995dc948
veris744 <71214562+veris744@users.noreply.github.com>
2025-05-03 14:23:25 +0200

    eol page

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3773551885"
+    serviceWorkerVersion: "368180673"
   }
 });
