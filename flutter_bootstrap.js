a2565daccc8a75dba9322a3697c54a3ed35b0776
Isabel DE VERGARA <idevergara@ncelrnd2915.nce.amadeus.net>
2025-05-28 16:17:09 +0200

    fix routing

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3736345020"
+    serviceWorkerVersion: "2077323904"
   }
 });
