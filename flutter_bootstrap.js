e6ee63c7f5b5921397f634f4f9aa4615d7da1944
veris744 <71214562+veris744@users.noreply.github.com>
2025-07-01 15:34:43 +0200

    social matchup details

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3521321286"
+    serviceWorkerVersion: "1789692304"
   }
 });
